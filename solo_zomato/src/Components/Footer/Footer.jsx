6656d6c84c7432e3a06e4be6296b3e9fe4c94877
Dhruv Bhardwaj <11dhruv.b@gmail.com>
2022-10-25 19:32:48 +0530

    final footer

--- solo_zomato/src/Components/Footer/Footer.jsx
+++ solo_zomato/src/Components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Dropdown, Image } from "react-bootstrap";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faFacebook, faInstagram, faTwitter, faYoutube, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 export function Footer() {
   return (
@@ -39,7 +41,7 @@ export function Footer() {
         </Dropdown>
       </Container>
       <Container className="d-flex mt-5">
-        <div className="font_footer w-25">
+        <div className="font_footer w-25 me-1">
           <h6 className="footer_header mb-3">About Zomato</h6>
           <div className="font-weight-light ul_font">
             <ul
@@ -55,7 +57,8 @@ export function Footer() {
             </ul>
           </div>
         </div>
-        <div className="font_footer w-25">
+
+        <div className="font_footer w-25 me-1">
           <h6 className="footer_header mb-3">Zomaverse</h6>
           <div className="font-weight-light ul_font">
             <ul
@@ -69,18 +72,52 @@ export function Footer() {
             </ul>
           </div>
         </div>
-        <div className="font_footer w-25">
-          <h6 className="footer_header mb-3">Zomaverse</h6>
+
+        <div className="font_footer w-25 me-1">
+          <h6 className="footer_header mb-3">For Restaurants</h6>
           <div className="font-weight-light ul_font">
             <ul
               className="bg-transparent w-100 footer_list px-0"
               variant="flush"
             >
-              <li>Zomato</li>
-              <li>Feeding India</li>
-              <li>Hyperpure</li>
-              <li>Zomaland</li>
+              <li>Patner With Us</li>
+              <li>App For You</li>
             </ul>
+          </div>
+          <h6 className="footer_header mb-3">For Enterprices</h6>
+          <div className="font-weight-light ul_font">
+            <ul
+              className="bg-transparent w-100 footer_list px-0"
+              variant="flush"
+            >
+              <li>Zomato For Work</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="font_footer w-25 me-1">
+          <h6 className="footer_header mb-3">Learn More</h6>
+          <div className="font-weight-light ul_font">
+            <ul
+              className="bg-transparent w-100 footer_list px-0"
+              variant="flush"
+            >
+              <li>Primary</li>
+              <li>Security</li>
+              <li>Terms</li>
+              <li>Sitemap</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="font_footer w-25 me-1">
+          <h6 className="footer_header mb-3">Social Links</h6>
+          <div>
+            <FontAwesomeIcon data-bs-toggle="tooltip" data-bs-placement="bottom" title="Linkedin" className="me-3 social_icon" icon={faLinkedin} />
+            <FontAwesomeIcon data-bs-toggle="tooltip" data-bs-placement="bottom" title="Instagram" className="me-3 social_icon" icon={faInstagram} />
+            <FontAwesomeIcon data-bs-toggle="tooltip" data-bs-placement="bottom" title="Twitter" className="me-3 social_icon" icon={faTwitter} />
+            <FontAwesomeIcon data-bs-toggle="tooltip" data-bs-placement="bottom" title="Youtube" className="me-3 social_icon" icon={faYoutube} />
+            <FontAwesomeIcon data-bs-toggle="tooltip" data-bs-placement="bottom" title="Facebook" className="me-0 social_icon" icon={faFacebook} />
           </div>
         </div>
       </Container>
